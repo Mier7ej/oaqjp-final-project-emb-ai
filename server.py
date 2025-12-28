931d145c4142c2d80ec02b6f50fd2d9f8f540e93
Mier7ej <mateusz.mierzejewski.96@gmail.com>
2025-12-28 03:41:19 -0500

    Your commit message describing the changes

---
+++ server.py
@@ -0,0 +1,35 @@
+#Import relevant functions.
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask(__name__)
+
+#Render homepage.
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route("/sentimentAnalyzer")
+def emotion_detector():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Return a formatted string
+    response = (
+        f"For the given statement, the system response is "
+        f"'anger': {result['anger']}, "
+        f"'disgust': {result['disgust']}, "
+        f"'fear': {result['fear']}, "
+        f"'joy': {result['joy']} and "
+        f"'sadness': {result['sadness']}. "
+        f"The dominant emotion is {result['dominant']}."
+    )
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
